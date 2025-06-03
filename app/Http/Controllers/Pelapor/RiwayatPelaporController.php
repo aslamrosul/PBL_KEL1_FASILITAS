f4cb5f22f09debc465f11d6b7160ea7f5f113506
aslamrosul <nvrsrndrmrx@gmail.com>
2025-06-03 10:06:38 +0700

    fix : ditolak ke riwayat

--- app/Http/Controllers/Pelapor/RiwayatPelaporController.php
+++ app/Http/Controllers/Pelapor/RiwayatPelaporController.php
@@ -30,26 +30,27 @@ class RiwayatPelaporController extends Controller
         ]);
     }
 
-    public function list(Request $request)
-    {
-        if ($request->ajax()) {
-            $laporan = LaporanModel::where('user_id', Auth::id())
-                ->where('status', ['selesai','ditolak'])
-                ->with(['periode', 'fasilitas', 'bobotPrioritas'])
-                ->select('t_laporan.*');
+   public function list(Request $request)
+{
+    if ($request->ajax()) {
+        $laporan = LaporanModel::where('user_id', Auth::id())
+            ->whereIn('status', ['selesai', 'ditolak']) // ini perbaikannya
+            ->with(['periode', 'fasilitas', 'bobotPrioritas'])
+            ->select('t_laporan.*');
 
-            return DataTables::of($laporan)
-                ->addIndexColumn()
-                ->addColumn('aksi', function ($laporan) {
-                    $showUrl = url('/pelapor/laporan/' . $laporan->laporan_id . '/show_ajax');
-                    return '
+        return DataTables::of($laporan)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($laporan) {
+                $showUrl = url('/pelapor/laporan/' . $laporan->laporan_id . '/show_ajax');
+                return '
                     <button onclick="modalAction(\'' . $showUrl . '\')" class="btn btn-info btn-sm">
                         <i class="fa fa-eye"></i> Detail
                     </button>
                 ';
-                })
-                ->rawColumns(['aksi'])
-                ->make(true);
-        }
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
     }
+}
+
 }
