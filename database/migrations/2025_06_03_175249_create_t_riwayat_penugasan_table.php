97ecb71cfb1af03d4b9136aedf4d8c48695d4cfd
aslamrosul <nvrsrndrmrx@gmail.com>
2025-06-05 14:40:45 +0700

    fix seeder

--- database/migrations/2025_06_03_175249_create_t_riwayat_penugasan_table.php
+++ database/migrations/2025_06_03_175249_create_t_riwayat_penugasan_table.php
@@ -9,19 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-   public function up()
-{
-    Schema::create('t_riwayat_penugasan', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('laporan_id')->constrained('laporan'); // ganti jika nama tabel berbeda
-        $table->foreignId('teknisi_id')->constrained('users');
-        $table->foreignId('sarpras_id')->constrained('users');
-        $table->date('tanggal_penugasan');
-        $table->string('status_penugasan')->default('ditugaskan'); // bisa juga enum
-        $table->text('catatan')->nullable();
-        $table->timestamps();
-    });
-}
+    public function up()
+    {
+        Schema::create('t_riwayat_penugasan', function (Blueprint $table) {
+            $table->id('riwayat_penugasan_id');
+            $table->unsignedBigInteger('laporan_id');
+            $table->unsignedBigInteger('teknisi_id');
+            $table->unsignedBigInteger('sarpras_id');
+            $table->date('tanggal_penugasan');
+            $table->string('status_penugasan')->default('ditugaskan'); // bisa juga enum
+            $table->text('catatan')->nullable();
+            $table->timestamps();
+
+            $table->foreign('teknisi_id')->references('user_id')->on('m_user');
+            $table->foreign('sarpras_id')->references('user_id')->on('m_user');
+            $table->foreign('laporan_id')->references('laporan_id')->on('t_laporan');
+
+        });
+    }
 
     /**
      * Reverse the migrations.
