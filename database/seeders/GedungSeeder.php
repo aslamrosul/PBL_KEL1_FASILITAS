d84251be929a3e81f5b9e20011ff2b9b75d41b17
Nuraayu <nurayu2718@gmail.com>
2025-06-11 10:05:12 +0700

    UPDATE : Seeder gedung

--- database/seeders/GedungSeeder.php
+++ database/seeders/GedungSeeder.php
@@ -11,67 +11,67 @@ class GedungSeeder extends Seeder
     {
         DB::table('m_gedung')->insert([
             [
-                'gedung_kode' => 'GDG-ADM',
+                'gedung_kode' => 'GD-ADM',
                 'gedung_nama' => 'Gedung Administrasi Niaga',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-GRAPOL',
+                'gedung_kode' => 'GD-GRAPOL',
                 'gedung_nama' => 'Gedung Graha Polinema (Grapol) gedung kuliah bersama',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-JTS',
+                'gedung_kode' => 'GD-JTS',
                 'gedung_nama' => 'Gedung Jurusan Teknik Sipil',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-TM',
+                'gedung_kode' => 'GD-TM',
                 'gedung_nama' => 'Gedung Jurusan Teknik Mesin',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AD-AKT',
+                'gedung_kode' => 'GD-AD-AKT',
                 'gedung_nama' => 'Gedung AD jurusan akuntansi',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AE-ADM',
+                'gedung_kode' => 'GD-AE-ADM',
                 'gedung_nama' => 'AE jurusan administrasiniaga',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AQ-KIM',
+                'gedung_kode' => 'GD-AQ-KIM',
                 'gedung_nama' => 'AQ jurusan kimia',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AF-ELEK',
+                'gedung_kode' => 'GD-AF-ELK',
                 'gedung_nama' => 'AF jurusan elektronika',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AB-ADM',
+                'gedung_kode' => 'GD-AB-ADM',
                 'gedung_nama' => 'AB jurusan administrasiniaga',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AJ-ELEK',
+                'gedung_kode' => 'GD-AJ-ELK',
                 'gedung_nama' => 'AJ jurusan elektronika',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'gedung_kode' => 'GDG-AK-ELEK',
+                'gedung_kode' => 'GD-AK-ELK',
                 'gedung_nama' => 'AK jurusan elektronika',
                 'created_at' => now(),
                 'updated_at' => now()
